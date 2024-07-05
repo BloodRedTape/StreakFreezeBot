f7a8d3d02ce80d3b8fb4dfbf610fc2432acd8332
I.Kostiuk <gefestroyale@gmail.com>
2024-07-05 16:28:17 +0300

    Calendar now is actually usable

--- mini_app_react/src/components/Calendar.tsx
+++ mini_app_react/src/components/Calendar.tsx
@@ -1,72 +1,98 @@
-import { List, Text } from '@xelene/tgui';
+import { Text } from '@xelene/tgui';
 import { CSSProperties } from 'react';
 
 type CalendarDayProps = {
     day: number
 }
 
-const CalendarDay: React.FC<CalendarDayProps> = ({ day }) => {
-    const isBlue = Math.random() > 0.7;
+enum DayType{
+    Empty,
+    None,
+    Freeze,
+    Commit
+}
 
-    const rectColor = isBlue ? '#4da9fa' : '#f59842';
+const GetColorFor = (type: DayType)=>{
+    switch (type) {
+        case DayType.None:
+            return 'red'
+        case DayType.Freeze:
+            return '#4da9fa'
+        case DayType.Commit:
+            return '#f59842'
+        case DayType.Empty:
+            return 'white'
+	}
+}
+
+
+const CalendarDay: React.FC<CalendarDayProps> = ({ day }) => {
+    let type: DayType = Math.random() > 0.7 ? DayType.Freeze : DayType.Commit;
+
+    if (day == 0 || day == null)
+        type = DayType.Empty;
 
     const containerStyle: CSSProperties = {
-    position: 'relative',
-    display: 'inline-block',
-    width: '40px',
-    height: '40px',
-    backgroundColor: rectColor,
-    borderRadius: '20%', // This makes the rectangle fully rounded / circle
+        position: 'relative',
+        display: 'inline-block',
+        width: '40px',
+        height: '40px',
+        backgroundColor: GetColorFor(type),
+        borderRadius: '20%',
     };
 
-  const textStyle: CSSProperties = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    color: 'white', // Color of the text, ensure it contrasts with the image
-    fontWeight: 'bold',
-    textAlign: 'center', // Ensure text inside the Text component is centered
+    if (type == DayType.Empty)
+        return (<div style={ containerStyle }></div>);
+
+    const textStyle: CSSProperties = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        color: 'white', // Color of the text, ensure it contrasts with the image
+        fontWeight: 'bold',
+        textAlign: 'center', // Ensure text inside the Text component is centered
     };
 
-  return (
-    <List>
-      <div style={containerStyle}>
-        <Text weight="3" style={textStyle}>{ day }</Text>
-      </div>
-      </List>
-  );
+    return (
+        <div style={containerStyle}>
+            <Text weight="3" style={textStyle}>{ day }</Text>
+        </div>
+    );
 };
 
 export const Calendar = () => {
-  const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-  const daysInMonth = 28; // assuming 4 weeks of 7 days
-  let daysArray = Array.from({ length: daysInMonth }, (_, i) => i + 1);
+    const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    const date = new Date(2024, 8, 0)
 
-  // Break daysArray into chunks of 7 to represent weeks
-  const weeks = [];
-  while (daysArray.length > 0) {
-    weeks.push(daysArray.splice(0, 7));
-  }
+    const daysInMonth = date.getDate();
+    const startWeekDay = (new Date(date.getFullYear(), date.getMonth(), 1).getDay() - 1) % 7;
 
-  return (
-    <table>
-      <thead>
-        <tr>
-          {daysOfWeek.map((day, index) => (
-            <th key={index}><Text weight="3">{day}</Text></th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {weeks.map((week, index) => (
-          <tr key={index}>
-            {week.map(day => (
-                <td><CalendarDay day={ day }/></td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
+    let daysArray = new Array(startWeekDay).fill(0).concat(Array.from({ length: daysInMonth }, (_, i) => i + 1));
+
+    const weeks = [];
+    while (daysArray.length > 0) {
+        weeks.push(daysArray.splice(0, 7));
+    }
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    {daysOfWeek.map((day, index) => (
+                        <th key={index}><Text weight="3">{day}</Text></th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                {weeks.map((week, index) => (
+                    <tr key={index}>
+                        {week.map(day => (
+                            <td><CalendarDay day={ day }/></td>
+                        ))}
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
 };
