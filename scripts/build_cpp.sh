abcdf1e97c5a3dd9ec663159fce9c9766f59379d
I.Kostiuk <gefestroyale@gmail.com>
2024-07-24 15:42:46 +0300

    Move all to conan

--- scripts/build_cpp.sh
+++ scripts/build_cpp.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 pushd ..
+
 mkdir -p build
+conan install . --build=missing -s build_type=Release
+
 pushd build
-cmake .. -DWITH_DEBUG_COMMANDS=0
+cmake ..  -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE:FILEPATH="./Debug/generators/conan_toolchain.cmake"  -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DCMAKE_BUILD_TYPE=Release -DWITH_DEBUG_COMMANDS=0
 
 make -j12 
 
