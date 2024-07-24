8cd0298bb593bc2256df698913ab5ed8493d1591
I.Kostiuk <gefestroyale@gmail.com>
2024-07-24 16:32:07 +0300

    no conan for build

--- scripts/build_cpp.sh
+++ scripts/build_cpp.sh
@@ -6,7 +6,7 @@ mkdir -p build
 conan install . --build=missing -s build_type=Release
 
 pushd build
-cmake ..  -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE:FILEPATH="./Debug/generators/conan_toolchain.cmake"  -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DCMAKE_BUILD_TYPE=Release -DWITH_DEBUG_COMMANDS=0
+cmake ..  -G "Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE:FILEPATH="./Debug/generators/conan_toolchain.cmake"  -DCMAKE_POLICY_DEFAULT_CMP0091=NEW -DCMAKE_BUILD_TYPE=Release -DWITH_DEBUG_COMMANDS=0 -DCMAKE_WITH_CONAN=0
 
 make -j12 
 
