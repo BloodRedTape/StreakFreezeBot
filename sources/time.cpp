2fd86d67855cf001dbfdc7b61cbb1534edd4b203
I.Kostiuk <gefestroyale@gmail.com>
2024-08-05 00:31:19 +0300

    move to simpler timezone handling

--- sources/time.cpp
+++ sources/time.cpp
@@ -1,16 +1,24 @@
 #include "time.hpp"
 #include <iostream>
 #include <ctime>
-#include <date/tz.h>
 
 namespace DateUtils{
 
     static Date NowImpl() {
         auto now = std::chrono::system_clock::now();
-        auto tz = std::chrono::current_zone();
-        auto zoned_now = date::make_zoned(tz, now);
-    
-        return std::chrono::floor<date::days>(zoned_now.get_sys_time());
+        auto time_t_now = std::chrono::system_clock::to_time_t(now);
+
+        // Use localtime or localtime_s to handle timezones
+        std::tm local_tm;
+#ifdef _WIN32
+        localtime_s(&local_tm, &time_t_now);
+#else
+        localtime_r(&time_t_now, &local_tm);
+#endif
+
+        // Convert tm to time_point<system_clock, days>
+        auto midnight = std::chrono::system_clock::from_time_t(std::mktime(&local_tm));
+        return std::chrono::floor<std::chrono::days>(midnight);
     }
 
 #if WITH_ADVANCE_DATE
